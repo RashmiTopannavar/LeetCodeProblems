8700399eb5eb5afd85144350a14e5f90f25aa73e
Rashmi Venkatesh Topannavar <140710741+RashmiTopannavar@users.noreply.github.com>
2024-06-06 15:53:19 -0500

    Time: 48 ms (38.61%), Space: 17.2 MB (47.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,9 +6,6 @@ class Solution:
             if (ord(s[i]) >= 97 and ord(s[i]) <= 122) or (ord(s[i]) >= 65 and ord(s[i]) <= 90) or (ord(s[i]) >= 48 and ord(s[i]) <= 57):
                 a+= s[i]
 
-        if a == a[::-1] :
-
-            return True
-        else:
-            return False          
+        return a == a[::-1]
+        
         
